18287c39ed98bf664b3ea03fd706db32201fcc01
이 영 종 <naki5087@naver.com>
2022-02-15 10:08:09 +0900

    로그 찍어보기

--- app/src/main/java/com/nepplus/kotlinbasic_20220215/MainActivity.kt
+++ app/src/main/java/com/nepplus/kotlinbasic_20220215/MainActivity.kt
@@ -12,7 +12,6 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
         btnLog.setOnClickListener {
             Log.d("MainActivity", "로그 버튼 눌림")
         }
