a35ea76556ae1094cc7689659a6a56d3c59ebb5d
이 영 종 <naki5087@naver.com>
2022-02-15 10:15:34 +0900

    비교용 버튼 추가, 주석 작성

--- app/src/main/java/com/nepplus/kotlinbasic_20220215/MainActivity.kt
+++ app/src/main/java/com/nepplus/kotlinbasic_20220215/MainActivity.kt
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.widget.Button
+import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -12,8 +13,17 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+//         btnLog가 눌리면 어떤 코드를 실행할지, 할 일을 설정.
         btnLog.setOnClickListener {
+
+//            로그를 찍어보는 기능 실행. => btnLog가 클릭될 때 마다 실행.
             Log.d("MainActivity", "로그 버튼 눌림")
+        }
+
+        btnToast.setOnClickListener {
+            Toast.makeText(this, "토스트 버튼 눌림", Toast.LENGTH_SHORT).show()
+            return@setOnClickListener
         }
     }
 }
