3e8a1bad8d5bfc319b43153dfac5457c87f3f515
이 영 종 <naki5087@naver.com>
2022-02-15 09:55:26 +0900

    화면에 버튼 추가

--- app/src/main/java/com/nepplus/kotlinbasic_20220215/MainActivity.kt
+++ app/src/main/java/com/nepplus/kotlinbasic_20220215/MainActivity.kt
@@ -2,10 +2,16 @@ package com.nepplus.kotlinbasic_20220215
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
     }
 }
